62ded76ceeb860fcec5b2b84efb220f41f7ac9d6
rayaneDZ <chlef-algerie@hotmail.fr>
2019-06-02 03:32:12 +0100

    added a log

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/user', userRoute);
 //============MIDDLEWARES===========//
 
 //========CONNECT TO DATABASE=======//
-console.log(process.env.MONGODB_USERNAME, process.env.MONGODB_PASSWORD)
+console.log(process.env, process.env)
 mongoose.connect('mongodb+srv://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@social-network-app-ovmhv.mongodb.net/social_network?retryWrites=true&w=majority', {useNewUrlParser: true}, (err, db) =>{
     mongoose.connection.readyState == 1 ? console.log('CONNECTED TO DB') : console.log('UNABLE TO CONNECT TO DB');
     if(err) {
